e7d703e62e028a31f55ed59f51610dfadc3e6672
vanthang24803 <nguyenthang24803@gmail.com>
2023-09-18 22:25:53 +0700

    Create Project LMS Platform NextJS 13

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='p-10'>
+      <UserButton afterSignOutUrl="/" />
+    </main>
+  );
+}
