8e06b7e9de81328d155b5c4e2378f533efd63a6d
vanthang24803 <nguyenthang24803@gmail.com>
2023-09-22 10:31:47 +0700

    Create API Course and Baisc UI Course ID

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface ActionsProps {
+  disabled: boolean;
+  courseId: string;
+  isPublished: boolean | undefined;
+}
+
+export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {};
+
+  const onDelete = async () => {};
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
